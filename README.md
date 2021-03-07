f5dc1e58d9c4cf1a6aa2cbc83f3afa92cd12b400
Robin Hayden <robin@robinhayden.com>
2021-03-07 12:34:21 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+***Activate the environment***
+```
+source blockchain-env/bin/activate
+```
+
+***Install dependencies***
+```
+pip3 install -r requirements.txt
+```
+
+**Run tests**
+```
+python3 -m pytest backend/tests
+```
---
+++ backend/blockchain/blockchain.py
@@ -0,0 +1,26 @@
+from backend.blockchain.block import Block
+
+class Blockchain:
+    """
+    Blockchain: a public ledger of transactions.
+    Implented as a list of blocks which represent the data of transactions
+    """
+    def __init__(self):
+        self.chain = [Block.genesis()]
+
+    def add_block(self, data):
+        self.chain.append(Block.mine_block(self.chain[-1], data))
+
+    def __repr__(self):
+        return f'Blockchain: {self.chain}'
+
+def main():
+    blockchain = Blockchain()
+    blockchain.add_block('one')
+    blockchain.add_block('two')
+
+    print(blockchain)
+    print(f'Blockchain name: {__name__}')
+
+if __name__ == '__main__':
+    main()
