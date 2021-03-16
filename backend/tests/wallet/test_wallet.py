785695dbd1a72eb34f24b1bc3d0534be41d4304e
Robin Hayden <robin@robinhayden.com>
2021-03-16 16:24:26 +0000

    connected the blockchain to the cryptocurrency

--- backend/tests/wallet/test_wallet.py
+++ backend/tests/wallet/test_wallet.py
@@ -1,4 +1,7 @@
 from backend.wallet.wallet import Wallet
+from backend.blockchain.blockchain import Blockchain
+from backend.config import STARTING_BALANCE
+from backend.wallet.transaction import Transaction
 import pytest
 
 @pytest.fixture
@@ -16,3 +19,38 @@ def test_verify_invalid_wallet_signature(data):
     signature = wallet.sign(data)
 
     assert not wallet.verify(Wallet().public_key, data, signature)
+
+def test_calculate_balance():
+    blockchain = Blockchain()
+    wallet = Wallet()
+
+    assert Wallet.calculate_balance(blockchain, wallet.address) == STARTING_BALANCE
+
+    amount = 50
+    transaction = Transaction(wallet, 'recipient', amount)
+    blockchain.add_block([transaction.to_json()])
+
+    assert Wallet.calculate_balance(blockchain, wallet.address) ==\
+        STARTING_BALANCE - amount
+
+    recieved_amount_1 = 25
+    recieved_transaction_1 = Transaction(
+        Wallet(),
+        wallet.address,
+        recieved_amount_1
+    )
+
+    recieved_amount_2 = 43
+    recieved_transaction_2 = Transaction(
+        Wallet(),
+        wallet.address,
+        recieved_amount_2
+    )
+
+    blockchain.add_block([
+        recieved_transaction_1.to_json(),
+        recieved_transaction_2.to_json()
+    ])
+
+    assert Wallet.calculate_balance(blockchain, wallet.address) ==\
+        STARTING_BALANCE - amount + recieved_amount_1 + recieved_amount_2
